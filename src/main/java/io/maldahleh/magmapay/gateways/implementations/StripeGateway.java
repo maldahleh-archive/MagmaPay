b709b3b3efd4fd0a4cef32ca820e507d1114b377
Mohammed Al-Dahleh <maldahleh@codeovo.io>
2017-04-30 23:11:18 -0400

    Inclusion of Stripe API

--- src/main/java/io/maldahleh/magmapay/gateways/implementations/StripeGateway.java
+++ src/main/java/io/maldahleh/magmapay/gateways/implementations/StripeGateway.java
@@ -1,6 +1,14 @@
 package io.maldahleh.magmapay.gateways.implementations;
 
+import com.stripe.Stripe;
+
 import io.maldahleh.magmapay.gateways.Gateway;
 
 public class StripeGateway extends Gateway {
+
+    public StripeGateway(String apiKey) {
+        Stripe.apiKey = apiKey;
+    }
+
+
 }
