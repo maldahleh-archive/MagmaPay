4c05d74d29516eaf51e4e59a2c9f65cb2174056f
Mohammed Al-Dahleh <maldahleh@codeovo.io>
2017-05-04 00:06:46 -0400

    Comment GatewayManager class

--- src/main/java/io/maldahleh/magmapay/gateways/Gateway.java
+++ src/main/java/io/maldahleh/magmapay/gateways/Gateway.java
@@ -2,7 +2,6 @@ package io.maldahleh.magmapay.gateways;
 
 import io.maldahleh.magmapay.charges.requests.ChargeRequest;
 import io.maldahleh.magmapay.charges.response.ChargeResponse;
-
 import io.maldahleh.magmapay.exceptions.GatewayMethodUnsupportedException;
 
 public abstract class Gateway {
--- src/main/java/io/maldahleh/magmapay/gateways/GatewayManager.java
+++ src/main/java/io/maldahleh/magmapay/gateways/GatewayManager.java
@@ -13,10 +13,22 @@ public class GatewayManager {
         this.gateways = new HashMap<>();
     }
 
+    /**
+     * Register a gateway for use.
+     *
+     * @param availableGateway the enum of the gateway for lookup
+     * @param gateway the implementation of the gateway
+     */
     public void registerGateway(AvailableGateway availableGateway, Gateway gateway) {
         gateways.put(availableGateway, gateway);
     }
 
+    /**
+     * Retrieve a gateway's implementation for use.
+     *
+     * @param gateway the enum of the gateway which needs to be retrieved
+     * @return the implementation of the gateway, null if not registered
+     */
     public Gateway getGatewayImplementation(AvailableGateway gateway) {
         return gateways.getOrDefault(gateway, null);
     }
