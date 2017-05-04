ba2ea3a39f75494d1bf53ee6c44c377de2542a97
Mohammed Al-Dahleh <maldahleh@codeovo.io>
2017-05-04 00:11:06 -0400

    Early charge failure and basic API

---
+++ src/main/java/io/maldahleh/magmapay/api/MagmaPayAPI.java
@@ -0,0 +1,11 @@
+package io.maldahleh.magmapay.api;
+
+import io.maldahleh.magmapay.MagmaPay;
+
+public class MagmaPayAPI {
+    private MagmaPay magmaPay;
+
+    public MagmaPayAPI(MagmaPay magmaPay) {
+        this.magmaPay = magmaPay;
+    }
+}
--- src/main/java/io/maldahleh/magmapay/charges/response/ChargeResponse.java
+++ src/main/java/io/maldahleh/magmapay/charges/response/ChargeResponse.java
@@ -1,4 +1,20 @@
 package io.maldahleh.magmapay.charges.response;
 
 public class ChargeResponse {
+    private boolean earlyFail;
+    private ChargeFailStatus chargeFailStatus;
+
+    /**
+     * Used when failure occurs prior to attempted communication with gateway.
+     *
+     * @param chargeFailStatus status of the failed charge
+     */
+    public ChargeResponse(ChargeFailStatus chargeFailStatus) {
+        this.earlyFail = true;
+        this.chargeFailStatus = chargeFailStatus;
+    }
+
+    public boolean isEarlyFail() { return earlyFail; }
+
+    public ChargeFailStatus getFailStatus() { return chargeFailStatus; }
 }
